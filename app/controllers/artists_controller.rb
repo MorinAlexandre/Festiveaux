bc858836e60230b39fefbbe65e3e1111bba64c18
Alexandre Morin <morinalexandrem@gmail.com>
2016-05-08 14:28:42 +0200

    template artist done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,7 +4,7 @@ class ArtistsController < ApplicationController
   # GET /artists
   # GET /artists.json
   def index
-    @artists = Artist.all
+    @artists = Artist.paginate(page: params[:page], per_page:6)
   end
 
   # GET /artists/1
