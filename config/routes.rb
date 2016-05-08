f7c8fc3c4bfdee749920758b07772c4cb91106e2
Alexandre Morin <morinalexandrem@gmail.com>
2016-05-08 16:09:31 +0200

    Menu contact

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :concerts
   resources :festivals
   root 'home#index'
+  get 'contact' => 'home#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
