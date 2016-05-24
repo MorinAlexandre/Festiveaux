555778ba813166b0e51344ff067483950df618d0
Alexandre Morin <morinalexandrem@gmail.com>
2016-05-24 23:55:34 +0200

    form admin

--- app/controllers/admin/festivals_controller.rb
+++ app/controllers/admin/festivals_controller.rb
@@ -11,9 +11,6 @@ class FestivalsController < AdminController
   # GET /festivals/1
   # GET /festivals/1.json
   def show
-    @concerts = @festival.concerts.paginate(page: params[:page], per_page:3)
-    @comment = Comment.new
-    @comments = @festival.comments.paginate(page: params[:page], per_page:5)
   end
 
   # GET /festivals/new
